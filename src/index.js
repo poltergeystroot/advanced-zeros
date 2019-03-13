23a37e4c1b8731dddac0578d2e3ed4f27015d2e1
Pavel Karatkevich <47752576+poltergeystroot@users.noreply.github.com>
2019-03-14 02:02:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  x=base;
+  let arr = [];
+  let rez = [];
+  if(x > 0 && x != 1){
+  for(let i = 2; i <= x; i++){
+    while(x % i == 0 && x > 1){
+      arr.push(i);
+      x = x / i;
+    }
+      if(x/i == 1)arr.push(x);
+    }
+  }
+for(let i = 0; i < arr.length; i++) {
+  result = 0;
+  num=number;
+  while (num >1){
+    num=Math.floor(num/arr[i]);
+    result += num;
+  }
+  var j, count = 0, len = arr.length;
+    for(j = 0; j < len; j++)
+      if(arr[j] == arr[i])
+        count++;
+  rez.push(Math.floor(result/count));
+}
+min = rez[0];
+var min_number;
+  for (i = 0; i < rez.length; ++i) {
+      if (rez[i] < min) {min = rez[i];}
+  }
+  return min;
 }
